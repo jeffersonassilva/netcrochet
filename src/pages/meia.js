c1b86174a15fd3114f3850b34ae2f0a6137ac45e
Jefferson Silva <jeffersonassilva@gmail.com>
2024-01-02 11:48:43 -0300

    feat: removendo arquivos desnecessários

--- src/pages/meia.js
+++
@@ -1,7 +0,0 @@
-import Base from "../components/Base";
-
-const Meia = () => {
-  return <Base titulo="Meia" />;
-};
-
-export default Meia;
--- src/pages/touca.js
+++
@@ -1,7 +0,0 @@
-import Base from "../components/Base";
-
-const Touca = () => {
-  return <Base titulo="Touca" />;
-};
-
-export default Touca;
